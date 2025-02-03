c5ff359e37cc81620cfcb8637c4da245f1dd1d90
Jean Dannemann Carone <jean.carone@gmail.com>
2025-02-03 13:54:24 -0300

    Tweaks.

--- src/main/java/prepare/datastructures/arrays/_1_arrays_ds/OneLoopSwap.java
+++ src/main/java/prepare/datastructures/arrays/_1_arrays_ds/OneLoopSwap.java
@@ -3,7 +3,7 @@ package prepare.datastructures.arrays._1_arrays_ds;
 import java.util.Collections;
 import java.util.List;
 
-class Fav_OneForLoopOnly extends AbstractSolution {
+class OneLoopSwap extends AbstractSolution {
 
     List<Integer> reverseArray(List<Integer> a) {
         for (int i = 0, d = a.size() - 1; i < d; i++, d--)
