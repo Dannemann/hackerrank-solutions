ef567502cec0e61d1ff77f8301105d4d4bcdf9a7
Jean Dannemann Carone <jean.carone@gmail.com>
2025-04-25 13:16:09 -0300

    Added prepare.algorithms.warmup._1_solve_me_first.

---
+++ src/main/java/prepare/algorithms/warmup/_1_solve_me_first/AbstractSolution.java
@@ -0,0 +1,8 @@
+package prepare.algorithms.warmup._1_solve_me_first;
+
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+abstract class AbstractSolution {
+
+	abstract int solveMeFirst(int a, int b);
+
+}
---
+++ src/main/java/prepare/algorithms/warmup/_1_solve_me_first/Solution.java
@@ -0,0 +1,11 @@
+package prepare.algorithms.warmup._1_solve_me_first;
+
+class Solution extends AbstractSolution {
+
+	int solveMeFirst(int a, int b) {
+
+
+		return 0;
+	}
+
+}
